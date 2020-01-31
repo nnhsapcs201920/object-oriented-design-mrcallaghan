ce5081987caa11b54fb0f2b619624b8a7ff00456
Matthew Callaghan <mcallaghan@Naperville203.org>
2020-01-30 21:19:19 -0600

    started FillInQuestion

---
+++ Question/FillInQuestion.java
@@ -0,0 +1,12 @@
+
+/**
+ * A FillInQuestion is constructed with a string that contains the answer surrounded by '_'.
+ *      For example, "The inventor of Java is _James Gosling_." The question should be displayed
+ *      as: The inventor of Java is _____. *
+ * @author mcallaghan
+ * @version 30 Jan 2020
+ */
+public class FillInQuestion extends Question
+{
+    
+}
