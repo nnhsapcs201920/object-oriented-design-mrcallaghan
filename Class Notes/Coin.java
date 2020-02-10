b601452546cbf56b6f81a99408ea29d8e3475857
Matt Callaghan <mcallaghan@naperville203.org>
2020-02-10 16:08:01 -0600

    implemented Comparable interface in Coin

--- Class Notes/Coin.java
+++ Class Notes/Coin.java
@@ -1,7 +1,7 @@
 /**
 A coin with a monetary value.
  */
-public class Coin
+public class Coin implements Comparable<Coin>
 {
     private double value;
     private String name;
@@ -33,6 +33,24 @@ public class Coin
     public String getName() 
     {
         return name;
+    }
+    
+    @Override
+    public int compareTo(Coin other)
+    {
+    
+        if (this.value > other.value)
+        {
+            return 1;
+        }
+        else if (this.value < other.value)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
     }
 
     /**
